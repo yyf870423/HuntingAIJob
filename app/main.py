82ee9c7488271f76d0e4c7c5ddb23cce4994fe83
yyf <y.e.yyf@163.com>
2025-07-02 18:31:04 +0800

    前端增加密码配置

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from app.gradio_ui import build_ui
+from app.config import get_config
 
 def launch_app():
+    config = get_config()
+    gradio_auth = config.get("gradio_auth", {})
     ui = build_ui()
-    ui.launch()
+    if gradio_auth.get("enable", False):
+        username = gradio_auth.get("username", "admin")
+        password = gradio_auth.get("password", "admin123")
+        ui.launch(auth=(username, password))
+    else:
+        ui.launch()
 
 if __name__ == "__main__":
     launch_app() 
